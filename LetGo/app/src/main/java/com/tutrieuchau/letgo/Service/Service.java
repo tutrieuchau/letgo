289cfb8c5a483c0390cc93d67bbba3ec7a56f628
CUBEVN\ductam <ductam@vn-cubesystem.com>
2017-06-26 18:02:35 +0700

    Service

--- LetGo/app/src/main/java/com/tutrieuchau/letgo/Service/Service.java
+++ LetGo/app/src/main/java/com/tutrieuchau/letgo/Service/Service.java
@@ -3,10 +3,13 @@ package com.tutrieuchau.letgo.Service;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -19,8 +22,10 @@ import java.net.URL;
 
 public class Service extends AsyncTask<JSONObject,Void,JSONObject> {
     private Context context;
-    public Service(Context context){
+    ServiceCallback serviceCallback;
+    public Service(Context context, ServiceCallback serviceCallback){
         this.context = context;
+        this.serviceCallback = serviceCallback;
     }
 
     @Override
@@ -49,11 +54,28 @@ public class Service extends AsyncTask<JSONObject,Void,JSONObject> {
                 writer.close();
                 os.close();
 
-                
+                if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    BufferedReader in=new BufferedReader(
+                            new InputStreamReader(
+                                    connection.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+                    in.close();
+                    connection.disconnect();
+                    return new JSONObject(sb.toString());
+                }
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -63,6 +85,8 @@ public class Service extends AsyncTask<JSONObject,Void,JSONObject> {
     @Override
     protected void onPostExecute(JSONObject result) {
         super.onPostExecute(result);
+        serviceCallback.onProcessComplete(result);
+        //TODO: Hide Load Process
     }
  private interface ServiceCallback{
      void onProcessComplete(JSONObject result);
